9d48f20405ab2b15b25e71e1231313e312eb6b0f
David Lorenzi <david.lorenzi92@gmail.com>
2016-01-30 12:21:58 +0100

    Some more modifies on the scenes

---
+++ Ggj2016/Assets/Scripts/General/PlayerManager.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+	public struct PlayerDisposition
+	{
+		public GameObject player;
+		public GameObject gameZone;
+		public bool arrived;
+	}
+
+	public GameObject[] players = new GameObject[4];
+	public GameObject[] gameZones = new GameObject[4];
+	PlayerDisposition[] disposition = new PlayerDisposition[4];
+	float speed = 2f;
+
+	public float levelTime = 5f;
+	float elapsedTime = 0f;
+
+	bool movePlayers = false;
+
+	// Use this for initialization
+	void Start () {
+		RandomizeArray (gameZones);
+		AssignPlayers ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (movePlayers) {
+			MovePlayers ();
+		}
+	}
+
+	void AssignPlayers (){
+		for (int i = 0; i < disposition.Length; i++) {
+			disposition [i].player = players [i];
+			disposition [i].gameZone = gameZones [i];
+			disposition [i].arrived = false;
+		}
+		movePlayers = true;
+	}
+
+	void MovePlayers(){
+		float step = speed * Time.deltaTime;
+		for (int i = 0; i < disposition.Length; i++) {
+			disposition [i].player.transform.position = Vector3.MoveTowards (disposition [i].player.transform.position, 
+																			disposition [i].gameZone.transform.position,
+																			step);
+			if (disposition [i].player.transform.position == disposition [i].gameZone.transform.position) {
+				disposition [i].arrived = true;
+			}
+		}
+		if (disposition [0].arrived && disposition [1].arrived && disposition [2].arrived && disposition [3].arrived) {
+			movePlayers = false;
+		}
+	}
+
+	void RandomizeArray(GameObject[] array){
+		for (int i = 0; i < array.Length; i++) {
+			GameObject tmp = array [i];
+			int r = Random.Range (i, array.Length);
+			array [i] = array [r];
+			array [r] = tmp;
+		}
+	}
+
+	void RotatePositions(){
+		int mode = Random.Range (0, 3);
+		switch (mode) {
+		case 0:
+			RotateLeft ();
+			break;
+		case 1:
+			RotateRight ();
+			break;
+		case 2:
+			RotateCross ();
+			break;
+		default:
+			break;
+		}
+	}
+
+	void RotateLeft(){
+		for (int i = 0; i < gameZones.Length-1; i++) {
+			GameObject tmp = gameZones [i];
+			gameZones [i] = gameZones [i + 1];
+			gameZones [i + 1] = tmp;
+		}
+	}
+		
+	void RotateRight(){
+	}
+
+	void RotateCross(){
+	}
+}
--- Ggj2016/Assets/Scripts/Rooms/Ironing.cs
+++ Ggj2016/Assets/Scripts/Rooms/Ironing.cs
@@ -7,24 +7,45 @@ public class Ironing : MonoBehaviour {
 	public Transform rightEnd;
 	public Transform correctPosition;
 	public Transform cursor;
+	public string playerButton;
 	public float cursorSpeed = 3f;
+	public GameObject result;
 	int direction = -1;
+	float offset = 0f;
+	bool correct = false;
 
 	// Use this for initialization
 	void Start () {
-		float cursorWidht = cursor.GetComponent<Renderer> ().bounds.size.x;
+		float cursorWidth = cursor.GetComponent<Renderer> ().bounds.size.x;
+		offset = cursorWidth / 2;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (cursor.position.x <= leftEnd.position.x) {
+		if (cursor.position.x - offset <= leftEnd.position.x) {
+			result.GetComponent<Renderer> ().material.color = Color.white;
 			direction = 1;
 		}
-		if (cursor.position.x >= rightEnd.position.x) {
+		if (cursor.position.x + offset >= rightEnd.position.x) {
+			result.GetComponent<Renderer> ().material.color = Color.white;
 			direction = -1;
 		}
 		cursor.Translate (new Vector3 (cursorSpeed, 0f, 0f) * Time.deltaTime * direction);
+		if (Input.GetButtonDown (playerButton)) {
+			if (correct) {
+				result.GetComponent<Renderer> ().material.color = Color.green;
+			} else {
+				result.GetComponent<Renderer> ().material.color = Color.red;
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll){
+		correct = true;
+	}
 
 
+	void OnTriggerExit2D(Collider2D coll){
+		correct = false;
 	}
 }
