6bc53e1e55725d93eeb318a17de30a353109a33b
David Lorenzi <david.lorenzi92@gmail.com>
2016-01-31 01:35:11 +0100

    Changes somewhere

---
+++ Ggj2016/Assets/Scripts/General/RewardManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RewardManager : MonoBehaviour {
+
+	public Image progressBar;
+	public float step = 20f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Increment(){
+		progressBar.rectTransform.sizeDelta += new Vector2(step, 0f);
+	}
+}
